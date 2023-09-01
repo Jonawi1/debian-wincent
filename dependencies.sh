4ab7421f4854b73198a223d82879600f7e96adcc
jonwin <jonatan.wincent@gmail.com>
2023-09-01 20:54:20 +0200

    added more programs and renamed dependencies to install.sh

--- dependencies.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt-get install neovim git make grep wget build-essential libx11-dev libxft-dev libxinerama-dev libxrandr-dev xorg
---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get install \
+	neovim \
+	git \
+	make \
+	grep \
+	wget \
+	build-essential \
+	libx11-dev \
+	libxft-dev \
+	libxinerama-dev \
+	libxrandr-dev \
+	xorg \
+	cloudflare-warp
