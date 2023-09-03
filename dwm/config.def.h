03d77e36698e0ffd04921d026a31c23af7059324
jonwin <jonatan.wincent@gmail.com>
2023-09-03 18:54:52 +0200

    Added dwm patches (center, ceterfirstwindow, exitmenu)

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -33,9 +33,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     iscentered  isfloating  CenterThisWindow?   monitor */
+  { "st",       NULL,       NULL,       0,            0,          0,          1,                  -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,          1,          0,                  -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,          0,          0,                  -1 },
 };
 
 /* layout(s) */
@@ -67,6 +68,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include "exitdwm.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -104,7 +106,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_e,      exitdwm,       {0} },
 };
 
 /* button definitions */
