5906bd3f5934ef28fcd3ba97f764efac13a016aa
Jonatan Wincent <jonatan.wincent@gmail.com>
2023-09-08 19:34:12 +0200

    Corrected spelling ststatus -> slstatus

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,4 @@
 /bin/bash -c "sleep 0.1&&xrandr --newmode "3440x1440_30.00" 196.25 3440 3600 3952 4464 1440 1443 1453 1468 -hsync +vsync&&xrandr --addmode Virtual-1 3440x1440_30.00&&xrandr -s 3440x1440_30.00"
 ~/.fehbg
 picom &
-ststatus &
+slstatus &
