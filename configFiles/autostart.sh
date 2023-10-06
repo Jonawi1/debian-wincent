064b056a1b724c5d0e7b498f99914d483a776a69
Jonwin <jonatan.wincent@gmail.com>
2023-10-06 21:14:49 +0200

    changed default wallpaper

--- configFiles/autostart.sh
+++ configFiles/autostart.sh
@@ -7,7 +7,7 @@ modeline=$(cvt $width $height $refresh | sed -n 's/^\Modeline //p')
 if test -f ~/.fehbg; then
 	~/.fehbg
 else
-	feh --bg-fill debian-wincent/img/IMG_20230802_151507.jpg
+	feh --bg-fill debian-wincent/img/Wallpaper.png
 fi
 picom &
 slstatus &
