52473f97dbd1ce31492b9169d12fc5b0efbd3240
Jonatan Wincent <c22jwt@cs.umu.se>
2024-01-13 03:03:03 +0100

    simpler bg set

--- configFiles/autostart.sh
+++ configFiles/autostart.sh
@@ -4,11 +4,7 @@
 #refresh=120
 #modeline=$(cvt $width $height $refresh | sed -n 's/^\Modeline //p')
 #/bin/bash -c "sleep 0.1 && xrandr --newmode $modeline && xrandr --addmode Virtual-1 ${width}x${height}_${refresh}.00 && xrandr -s ${width}x${height}_${refresh}.00"
-if test -f ~/.fehbg; then
-	~/.fehbg
-else
-	feh --bg-fill debian-wincent/img/black.png
-fi
+feh --no-fehbg --bg-fill debian-wincent/img/black.png
 picom &
 slstatus &
 unclutter &
