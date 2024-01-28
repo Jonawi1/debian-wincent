062b65f0aff0fbb482c567a1754dbe45ff59a873
jonwin <jonwin@jonwin.se>
2024-01-28 16:52:19 +0100

    update battery notif

--- configFiles/battery-alert.sh
+++ configFiles/battery-alert.sh
@@ -10,13 +10,13 @@ while true; do
     BAT_STATE="grep "STATUS" /sys/class/power_supply/BAT0/uevent | awk -F = '{print $2}'"
     BAT_PERCENT=/sys/class/power_supply/BAT0/capacity
 
-    if [ $bat_state = 'Discharging' ]; then
-        if [ $bat_percent -lt 30 ]; then
-            dunstify --urgency=CRITICAL "Battery Low" "Level: ${bat_percent}%"
+    if [ $BAT_STATE = 'Discharging' ]; then
+        if [ $BAT_PERCENT -lt 30 ]; then
+            dunstify --urgency=CRITICAL "Battery Low" "Level: ${BAT_PERCENT}%"
         fi
     else
-        if [ $bat_percent -ge 100 ]; then
-            dunstify --urgency=NORMAL "Battery Full" "Level: ${bat_percent}%"
+        if [ $BAT_PERCENT -ge 100 ]; then
+            dunstify --urgency=NORMAL "Battery Full" "Level: ${BAT_PERCENT}%"
         fi
     fi
 
