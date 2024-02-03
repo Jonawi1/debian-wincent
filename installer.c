2eb32c430eba78773b019274c32c54e05fba402c
Jonatan Wincent <jonatan.wincent@proton.me>
2024-02-03 22:21:25 +0100

    mv fork to function

--- installer.c
+++ installer.c
@@ -10,47 +10,52 @@
 #include "config.h"
 
 /* function declarations */
-void installpackages(void);
+static void installpackages(void);
 
 /* variables */
+static int nchild = 0;
 
 /* function implementations */
 void
 installpackages(void)
 {
-    int npkgs = sizeof(packages) / sizeof(char*);
-    char *cmd[npkgs + 5];
+    pid_t p = fork();
+    if (p == 0) {
+        int npkgs = sizeof(packages) / sizeof(char*);
+        char *cmd[npkgs + 5];
 
-    cmd[0] = "sudo";
-    cmd[1] = "apt-get";
-    cmd[2] = "install";
-    cmd[3] = "-y";
-    cmd[npkgs + 4] = (char*)NULL;
-    
-    for (int i = 0; i < npkgs; i++) {
-        int size = strlen(packages[i]) * sizeof(char) + 1;
+        cmd[0] = "sudo";
+        cmd[1] = "apt-get";
+        cmd[2] = "install";
+        cmd[3] = "-y";
+        cmd[npkgs + 4] = (char*)NULL;
 
-        cmd[i + 4] = (char*) malloc(size);
-        if (cmd[i + 4] == NULL) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
+        for (int i = 0; i < npkgs; i++) {
+            int size = strlen(packages[i]) * sizeof(char) + 1;
+
+            cmd[i + 4] = (char*) malloc(size);
+            if (cmd[i + 4] == NULL) {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+            }
+            strcpy(cmd[i + 4], packages[i]);
         }
-        strcpy(cmd[i + 4], packages[i]);
-    }
 
-    printf("Sudo privileges are required for packages installation.\n");
-    execvp(cmd[0], cmd);
+        printf("Sudo privileges are required for packages installation.\n");
+        execvp(cmd[0], cmd);
+    }
+    nchild++;
 }
 
 
 int 
 main(void)
 {
-    pid_t p = fork();
-    if (p == 0) {
-        installpackages();
+    installpackages();
+
+    for (int i = 0; i < nchild; i++) {
+        wait(NULL);
     }
-    wait(NULL);
 
     return EXIT_SUCCESS;
 }
