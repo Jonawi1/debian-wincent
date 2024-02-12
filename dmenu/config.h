e889438d24fee8d7c0c0109c8d1e9ee9f6146a89
jonwin <jonwin@jonwin.se>
2024-02-12 20:36:19 +0100

    dmenue xresources

--- dmenu/config.h
+++ dmenu/config.h
@@ -7,16 +7,24 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "monospace:size=10";
 static const char *fonts[] = {
-	"FiraCodeNerdFont:size=16"
+    font,
+	"monospace:size=10",
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#0e0e14" },
-	[SchemeSel] = { "#eeeeee", "#221627" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-    [SchemeBorder] = { "#388697", NULL},
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char bordercolor[] = "#388697";
+static char *colors[SchemeLast][2] = {
+	/*                  fg              bg          */
+    [SchemeNorm]    = { normfgcolor,    normbgcolor },
+    [SchemeSel]     = { selfgcolor,     selbgcolor  },
+    [SchemeOut]     = { "#000000",      "#00ffff"   },
+    [SchemeBorder]  = { bordercolor,    NULL        },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
@@ -35,3 +43,18 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 2;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",           STRING, &font },
+	{ "foreground",     STRING, &normfgcolor },
+	{ "background",     STRING, &normbgcolor },
+	{ "foreground",     STRING, &selfgcolor },
+	{ "color1",         STRING, &selbgcolor },
+	{ "color1",         STRING, &bordercolor },
+/*	{ "prompt",         STRING, &prompt }, */
+/*  { "alpha",          FLOAT,  &alpha }, */
+};
+
